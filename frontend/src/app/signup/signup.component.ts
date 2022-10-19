9b3bdb11c298886bfddd47da65fbd624bc26f586
Chirag Chaplot <chiragchaplot@gmail.com>
2022-10-20 07:43:24 +1100

    Sign Up Page

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
+import { GlobalConstants } from '../shared/global-constants';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +14,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: any  = FormGroup;
+  responseMessage: any;
+
+  constructor(private formBuilder: FormBuilder,
+    private router:Router,
+    private userServices:UserService,
+    private snackBar: SnackbarService,
+    private dialogReference: MatDialogRef<SignupComponent>,
+    private ngxService: NgxUiLoaderService) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      name:[null,[Validators.required,Validators.pattern(GlobalConstants.nameRegex)]],
+      email:[null,[Validators.required,Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber:[null,[Validators.required,Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      password:[null,[Validators.required]]
+    })
+  }
+
+  handleSubmit() {
+    this.ngxService.start()
+    var formData = this.signUpForm.value;
+    var data = {
+      name: formData.name,
+      email: formData.email,
+      contactNumber: formData.contactNumber,
+      password: formData.password
+    }
+
+    this.userServices.signup(data).subscribe((response:any) => {
+      this.ngxService.stop();
+      this.dialogReference.close();
+      this.responseMessage = response.message;
+      this.snackBar.openSnackBar(this.responseMessage,"");
+      this.router.navigate(['/'])
+    },(error) => {
+      this.ngxService.stop();
+      if(error.error.message) {
+        this.responseMessage = error.error.message
+      } else {
+        this.responseMessage = GlobalConstants.genericError
+      }
+      this.snackBar.openSnackBar(this.responseMessage,GlobalConstants.error);
+    });
   }
 
 }
