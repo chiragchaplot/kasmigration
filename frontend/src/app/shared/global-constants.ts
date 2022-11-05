717c111165470ee139ba3909c595da4c03512d1a
Chirag Chaplot <chiragchaplot@gmail.com>
2022-11-05 21:59:16 +1100

    Simple UI Changes

--- frontend/src/app/shared/global-constants.ts
+++ frontend/src/app/shared/global-constants.ts
@@ -1,6 +1,7 @@
 export class GlobalConstants {
     //Message
     public static genericError: string = "Something went wrong. Please try again later";
+    public static unauthorised: string = "You are not authorized to access this page";
     //Regex
     public static nameRegex: string = "[a-zA-Z0-9 ]*";
     public static emailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}"
