0f9cffdc81261165d726bfb6941e5a33fd108ece
Chirag Chaplot <chiragchaplot@gmail.com>
2022-11-09 08:43:44 +1100

    Added Course Service

---
+++ frontend/src/app/services/course/course.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  url = environment.apiUrl;
+  constructor(private httpClient: HttpClient) { }
+
+  updateStatus(data:any) {
+    return this.httpClient.post(this.url+"/course/updateuniversitycourse",data,{
+      headers: new HttpHeaders().set('Content-Type',"application/json")
+    })
+  }
+
+
+}
--- frontend/src/app/shared/menu-items.ts
+++ frontend/src/app/shared/menu-items.ts
@@ -13,6 +13,54 @@ const MENUITEMS = [
         name: 'Dashboard',
         icon: 'dashboard',
         role: ''
+    },
+    {
+        state: 'searchCourse',
+        name: 'Search Courses',
+        icon: ' list_alt',
+        role: ''
+    },
+    {
+        state: 'viewApplications',
+        name: 'View Applications',
+        icon: 'desktop_mac',
+        role: ''
+    },
+    {
+        state: 'inactiveAccounts',
+        name: 'Inactive Accounts',
+        icon: 'warning',
+        role: 'consultant'
+    },
+    {
+        state: 'inactiveAccounts',
+        name: 'Inactive Accounts',
+        icon: 'warning',
+        role: 'admin'
+    },
+    {
+        state: 'matchedStudents',
+        name: 'Matched Students',
+        icon: 'group',
+        role: 'admin'
+    },
+    {
+        state: 'unmatchedStudents',
+        name: 'Unmatched Students',
+        icon: 'group-add',
+        role: 'admin'
+    },
+    {
+        state: 'createApplication',
+        name: 'Create Application',
+        icon: 'note_add',
+        role: 'student'
+    },
+    {
+        state: 'uploadDocument',
+        name: 'Upload Document',
+        icon: 'cloud_upload',
+        role: 'student'
     }
 ];
 
